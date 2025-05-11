ee016e278354033b0c4ee53e8e19d91fb12a4b63
keemothy <kimchu@berkeley.edu>
2025-05-11 11:38:39 -0700

    Revert "changed some server logistics"
    This reverts commit 19e26c6f279ca099bf094c37b9699c77c6ee045d.
    changing this back since server failed

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ require('dotenv').config();
 const express = require('express');
 const fetch = require('node-fetch');
 const cors = require('cors');
-const path = require('path');
 
 const app = express();
 const PORT = 3000;
@@ -58,12 +57,6 @@ app.get('/api/celltower', async (req, res) => {
     console.error(err);
     res.status(500).json({ error: 'Failed to fetch cell tower info' });
   }
-});
-
-app.use(express.static(path.join(__dirname, 'project', 'dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'project', 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
